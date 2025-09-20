1b7fbdf92c273609c9622e7d928407c753762730
Milk-Cool <coder.tomsk@gmail.com>
2025-09-20 10:02:17 +0300

    ping the max server to keep the connection alive

--- src/max.ts
+++ src/max.ts
@@ -64,6 +64,7 @@ export class Client {
 
     private seq: number = 0;
     private ws: WebSocket;
+    private interval: ReturnType<typeof setInterval> | null = null;
 
     private handler: ((packet: Packet) => void) | null = null;
 
@@ -77,6 +78,8 @@ export class Client {
             if(this.handler !== null) this.handler(incoming);
         });
         await new Promise(resolve => this.ws.addEventListener("open", resolve));
+        if(this.interval !== null) clearInterval(this.interval);
+        this.interval = setInterval(async () => this.ping(), 10000);
     }
 
     private constructPacket(opcode: number, payload: object | null, seq: number = -1, direction: 0 | 1 = 0): Packet {
@@ -199,5 +202,9 @@ export class Client {
             from: Date.now(),
             getMessages: true
         })).payload as Messages;
+    }
+
+    private async ping() {
+        await this.msg(1, { interactive: true }, false);
     }
 }
